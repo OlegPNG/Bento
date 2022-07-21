204f9d423087e634855bf8650a08ac66186da585
OlegPNG <53950070+OlegPNG@users.noreply.github.com>
2022-07-21 19:47:08 -0400

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: '3d4ce657c76ca0b6c4dc302a17f409d1', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'F', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
